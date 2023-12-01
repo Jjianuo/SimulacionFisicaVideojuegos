4c72bda4f09f6ba528446d7f89ab441716567b9a
Jjianuo <Jwen@ucm.es>
2023-12-01 09:57:02 +0100

    Rigidbody

---
+++ skeleton/RigidBody.cpp
@@ -0,0 +1,16 @@
+#include "RigidBody.h"
+
+extern PxPhysics* gPhysics;
+extern PxScene* gScene;
+
+RigidBody::RigidBody(PxVec3 pos, double m, PxShape* s, PxVec4 c, PxMaterial* mat) 
+{
+	rB = gPhysics->createRigidDynamic(PxTransform(pos));
+	rB->setMass(m);
+	s->setMaterials(&mat, 1);
+	rB->attachShape(*s);
+	RenderItem* ri = new RenderItem(s, rB, c);
+	gScene->addActor(*rB);
+
+	//PxRigidBodyExt::updateMassAndInertia(rB, m * s.);
+}
---
+++ skeleton/RigidBody.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <PxPhysicsAPI.h>
+#include "RenderUtils.hpp"
+
+using namespace physx;
+
+class RigidBody
+{
+public:
+	RigidBody(PxVec3 pos, double m, PxShape* s, PxVec4 c, PxMaterial* mat);
+
+	PxRigidDynamic* rB;
+};
+
